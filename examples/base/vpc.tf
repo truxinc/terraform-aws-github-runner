5b6fa72c106ec3366897c1523554e267cd3bb710
Gayatri Katare <gayatrikatare111@gmail.com>
2023-10-11 16:24:58 +0530

    DOPS-1166 : base configuration added

--- examples/base/vpc.tf
+++ examples/base/vpc.tf
@@ -3,11 +3,11 @@ module "vpc" {
   version = "5.0.0"
 
   name = "${var.prefix}-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "11.0.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["11.0.1.0/24", "11.0.2.0/24"]
+  public_subnets  = ["11.0.101.0/24", "11.0.102.0/24"]
 
   enable_dns_hostnames    = true
   enable_nat_gateway      = true
